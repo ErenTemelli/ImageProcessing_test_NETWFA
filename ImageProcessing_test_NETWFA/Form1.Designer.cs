d074a5d6f1090ed058aeb33e63763d8aa2081bff
polestarising@gmail.com <polestarising@gmail.com>
2022-03-18 16:19:23 +0300

    image comparision between 2 pictures (pixel by pixel) done b.e.

--- ImageProcessing_test_NETWFA/Form1.Designer.cs
+++ ImageProcessing_test_NETWFA/Form1.Designer.cs
@@ -34,6 +34,8 @@ namespace ImageProcessing_test_NETWFA
             this.button1 = new System.Windows.Forms.Button();
             this.progressBar1 = new System.Windows.Forms.ProgressBar();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.openFileDialog2 = new System.Windows.Forms.OpenFileDialog();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -46,6 +48,7 @@ namespace ImageProcessing_test_NETWFA
             this.linkLabel1.TabIndex = 0;
             this.linkLabel1.TabStop = true;
             this.linkLabel1.Text = "Resim 1\'i seçin";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
             // 
             // linkLabel2
             // 
@@ -56,6 +59,7 @@ namespace ImageProcessing_test_NETWFA
             this.linkLabel2.TabIndex = 1;
             this.linkLabel2.TabStop = true;
             this.linkLabel2.Text = "Resim 2\'i seçin";
+            this.linkLabel2.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel2_LinkClicked);
             // 
             // button1
             // 
@@ -65,6 +69,7 @@ namespace ImageProcessing_test_NETWFA
             this.button1.TabIndex = 2;
             this.button1.Text = "Karşılaştır";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // progressBar1
             // 
@@ -81,6 +86,14 @@ namespace ImageProcessing_test_NETWFA
             this.pictureBox1.TabIndex = 4;
             this.pictureBox1.TabStop = false;
             // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // openFileDialog2
+            // 
+            this.openFileDialog2.FileName = "openFileDialog2";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
@@ -93,6 +106,7 @@ namespace ImageProcessing_test_NETWFA
             this.Controls.Add(this.linkLabel1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -106,6 +120,8 @@ namespace ImageProcessing_test_NETWFA
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.ProgressBar progressBar1;
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog2;
     }
 }
 
--- ImageProcessing_test_NETWFA/Form1.cs
+++ ImageProcessing_test_NETWFA/Form1.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+
 
 namespace ImageProcessing_test_NETWFA
 {
@@ -16,5 +18,77 @@ namespace ImageProcessing_test_NETWFA
         {
             InitializeComponent();
         }
+        string fname1, fname2;
+        int count1, count2, total;
+        bool flag = true;
+        double performans;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            progressBar1.Visible = false;
+            pictureBox1.Visible = false;
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            openFileDialog1.FileName = "";
+            openFileDialog1.Title = "Images";
+            openFileDialog1.Filter = "All Images|*.jpg; *.bmp; *.png";
+            openFileDialog1.ShowDialog();
+            if (openFileDialog1.FileName.ToString() != "")
+            {
+                fname1 = openFileDialog1.FileName.ToString();
+            }
+        }
+        private void linkLabel2_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            openFileDialog2.FileName = "";
+            openFileDialog2.Title = "Images";
+            openFileDialog2.Filter = "All Images|*.jpg; *.bmp; *.png";
+            openFileDialog2.ShowDialog();
+            if (openFileDialog2.FileName.ToString() != "")
+            {
+                fname2 = openFileDialog2.FileName.ToString();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            progressBar1.Visible = true;
+            string img1_ref, img2_ref;
+            Bitmap img1 = new Bitmap(fname1);
+            Bitmap img2 = new Bitmap(fname2);
+            progressBar1.Maximum = img1.Width;
+            if (img1.Width == img2.Width && img1.Height == img2.Height)
+            {
+                for (int i = 0; i < img1.Width; i++)
+                {
+                    for (int j = 0; j < img1.Height; j++)
+                    {
+                        img1_ref = img1.GetPixel(i, j).ToString();
+                        img2_ref = img2.GetPixel(i, j).ToString();
+                        if (img1_ref != img2_ref)
+                        {
+                            count2++;
+                            flag = false;
+                            break;
+                        }
+                        count1++;
+                    }
+                    progressBar1.Value++;
+                }
+
+                total = count1 + count2;
+                performans = 100 * count1 / total;
+                    MessageBox.Show(+ count1 + " adet aynı pixel ve " + count2 + " adet farklı pixel bulundu \n 2 fotoğraftaki benzerlik oranı: %" + performans );
+            }
+            else
+                MessageBox.Show("fotoğraflar karşılaştırılamaz");
+            this.Dispose();
+        }
     }
-}
+
+
+    }
+
+
